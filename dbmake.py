f518b49901ede4c44ad2c8d1b761c99e81175bf0
kimError <alswjd7961@naver.com>
2021-04-20 10:54:20 +0900

    python 으로  mysql 연결하는 원시 코드

---
+++ dbmake.py
@@ -0,0 +1,18 @@
+import pymysql
+
+# db 연결해주는 명령어
+db = pymysql.connect(
+    host = 'localhost',
+    port = 3306,
+    user = 'root',
+    passwd = '1234'
+    db = 'busan'
+)
+
+cursor = db.cursor() # 쿼리문을 날려주기 준비해주는 칭구
+# 쿼리문 db 에 날려주는 문장
+cursor.excute('SELECT * FROM users;')
+# 쿼리문 결과를 받아와주는 문장
+users = cursor.fetchall()
+# 결과보쟈아ㅏ
+print(users)
