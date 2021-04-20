2002552f141d323c3011d96f4fd259e35514d657
kimError <alswjd7961@naver.com>
2021-04-20 12:17:04 +0900

    python 변수 db에 넣기 원시 코드 실행가능

--- dbmake.py
+++ dbmake.py
@@ -35,8 +35,29 @@ sql_4 = '''
     VALUES 
     ('min', 'als@n', 'errorKim', 'jamwa');
 '''
-# 쿼리문을 날려주기 준비해주는 칭구
+
+# title = input('제목을 적으세요')
+# body = input("내용을 적으세요")
+# author = input("누구세요")
+# input_data = [title,body,author]
+# sql_5 = '''
+#     INSERT INTO `topic` 
+#     (`title`, `body`, `author`) 
+#     VALUES 
+#     (%s, %s, %s);
+# '''
+
+
+# 쿼리문을 날려주기 준비해주는 칭구(공통)
 cursor = db.cursor() 
+
+# 파이썬 변수 db에 넣기
+# 커서문 db에 날려주기
+# cursor.execute(sql_5,input_data)
+# # db적용
+# db.commit()
+# # db 연결 죽임
+# db.close()
 
 # 커서문 db에 날려주기
 # cursor.execute(sql_4)
@@ -47,7 +68,8 @@ cursor = db.cursor()
 
 
 # 쿼리문 db 에 날려주는 문장 -> execute(쿼리문)
-cursor.execute('SELECT * FROM users;')
+# cursor.execute('SELECT * FROM users;')
+cursor.execute('SELECT * FROM topic;')
 # cursor.execute(sql_3)
 # 쿼리문 결과를 받아와주는 문장
 users = cursor.fetchall()
