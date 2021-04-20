e84a5f949ca1bd933240c66edf6865b20d75c89d
kimError <alswjd7961@naver.com>
2021-04-20 11:14:04 +0900

    mysql 쿼리날리는 원시 코드

--- dbmake.py
+++ dbmake.py
@@ -8,11 +8,27 @@ db = pymysql.connect(
     password = '1234',
     db = 'busan'
 )
-
-cursor = db.cursor() # 쿼리문을 날려주기 준비해주는 칭구
+sql = '''
+    CREATE TABLE `topic` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`title` varchar(100) NOT NULL,
+	`body` text NOT NULL,
+	`author` varchar(30) NOT NULL,
+    `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+	) ENGINE=innoDB DEFAULT CHARSET=utf8;
+'''
+# 쿼리문을 날려주기 준비해주는 칭구
+cursor = db.cursor() 
+# 커서문 db에 날려주기
+cursor.execute(sql)
+# 적용
+db.commit()
+# 커서 커밋 죽임
+db.close()
 # 쿼리문 db 에 날려주는 문장 -> execute(쿼리문)
-cursor.execute('SELECT * FROM users;')
+# cursor.execute('SELECT * FROM users;')
 # 쿼리문 결과를 받아와주는 문장
-users = cursor.fetchall()
+# users = cursor.fetchall()
 # 결과보쟈아ㅏ
-print(users)
+# print(users)
